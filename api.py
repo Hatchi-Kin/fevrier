fa26810894fae4ae365a98f5ca14db4d16a44168
Hatchi-Kin <j.needham@hotmail.fr>
2024-03-19 00:50:53 +0100

    .env

--- api.py
+++ api.py
@@ -8,6 +8,8 @@ from pydantic import BaseModel, Field
 import boto3
 from dotenv import load_dotenv
 
+load_dotenv()
+
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 REGION_NAME = os.getenv("REGION_NAME")
