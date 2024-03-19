953cf6a0e764ad250277d563f3222db8d9d72ab2
Hatchi-Kin <j.needham@hotmail.fr>
2024-03-19 01:38:19 +0100

    .env

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 from unittest import TestCase, main
 from fastapi.testclient import TestClient
-from api import app
 import os
 
 
@@ -18,34 +17,6 @@ class TestDev(TestCase):
     def test_gitignore(self):
         list_files = os.listdir()
         self.assertIn(".gitignore", list_files)
-
-
-# class TestAPI(TestCase):
-
-#     # Vérifie que l'API est bien lancée
-#     def test_api(self):
-#         client = TestClient(app)
-#         response = client.get("/")
-#         self.assertEqual(response.status_code, 200)
-
-    # # Vérifie le endpoint predict
-    # def test_predict(self):
-    #     client = TestClient(app)
-    #     payload = {
-    #         "PassengerId": 892,
-    #         "Pclass": 3,
-    #         "Sex": 1,
-    #         "Age": 34.5,
-    #         "SibSp": 0,
-    #         "Parch": 0,
-    #         "Fare": 7.8292,
-    #         "Embarked": 1,
-    #     }
-    #     response = client.post("/predict", json=payload)
-    #     self.assertEqual(response.status_code, 200)
-
-    #     response2 = client.post("/predict2", json=payload)
-    #     self.assertEqual(response2.status_code, 200)
 
 
 if __name__ == "__main__":
